79a4b7b17af3fd18d052eae555e6abf6540b0cc6
Aditya Jain <aditya.jain@preciousinfosystem.com>
2023-02-14 13:47:41 +0530

    14/02/23 pages design

--- js/CustomSwiper.js
+++ js/CustomSwiper.js
@@ -1,11 +1,19 @@
 var mySwiper = new Swiper(".swiper-container", {
   speed: 400,
   initialSlide: 0,
-  // loop: true,
+  loop: true,
   autoplay: 5000,
   effect: "slide",
   spaceBetween: 0,
-  slidesPerView:3,
+  slidesPerView: 1,
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
   slidesOffsetBefore: 0,
   grabCursor: true,
   navigation: {
